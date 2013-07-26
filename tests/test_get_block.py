5abe86c39ed9359e234434ffe5902b2b2f356406
Rex <fdrex1987@gmail.com>
2013-07-26 23:52:47 +0800

    local method tested

--- tests/test_get_block.py
+++ tests/test_get_block.py
@@ -21,19 +21,26 @@ if __name__ == "__main__":
     #print block
     tx_ids2 = [str(tx.hash) for tx in block.transactions]
 
+    print 'getting block 247326 from local bitcoind ...'
+    block = pybit.get_block_by_height(247326, source=settings.SOURCE_LOCAL, test_net=False)
+    #print block
+    tx_ids3 = [str(tx.hash) for tx in block.transactions]
+    print len(block.transactions)
+
     print tx_ids1
     print tx_ids2
-    print 'compare transaction hashes one by one', all([e == tx_ids2[i] for i, e in enumerate(tx_ids1)])
-    print 'compare as two sets', set(tx_ids1) == set(tx_ids2)
+    print tx_ids3
+    print 'compare transaction hashes one by one', all([e == tx_ids2[i] == tx_ids3[i] for i, e in enumerate(tx_ids1)])
+    print 'compare as three sets', set(tx_ids1) == set(tx_ids2) == set(tx_ids3)
 
-    print 'getting block 80801 (testnet) from blockexplorer.com ...'
-    block = pybit.get_block_by_height(80801, source=settings.SOURCE_BLOCKEXPLORER_COM, test_net=True)
-    #print block
-    print len(block.transactions)
-
-    print 'getting block 80801 (testnet) from local bitcoind ...'
-    block = pybit.get_block_by_height(80801, source=settings.SOURCE_LOCAL, test_net=True)
-    #print block
-    print len(block.transactions)
+    # print 'getting block 80801 (testnet) from blockexplorer.com ...'
+    # block = pybit.get_block_by_height(80801, source=settings.SOURCE_BLOCKEXPLORER_COM, test_net=True)
+    # #print block
+    # print len(block.transactions)
+    #
+    # print 'getting block 80801 (testnet) from local bitcoind ...'
+    # block = pybit.get_block_by_height(80801, source=settings.SOURCE_LOCAL, test_net=True)
+    # #print block
+    # print len(block.transactions)
 
     print 'finished'
